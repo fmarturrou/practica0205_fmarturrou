08879b0bf3b7296b27e170e9f112abfdc0c45be0
fmarturrou <fmarturrou1@educacion.navarra.es>
2024-11-08 08:45:24 +0100

    Ejercicio

---
+++ Ejercicio01.py
@@ -0,0 +1,9 @@
+def saludo(nombre):
+    """Función de imprime un saludo con el nombre que pongas
+    El nombre que pones en la invocación de la función es el que
+    se imprime en la cadena ¡Hola <nombre>!
+    """
+
+    print(f"¡Hola {nombre}!")
+
+saludo("Santi")
